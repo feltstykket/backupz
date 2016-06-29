80d9240875e29b0426375c957f4c09a771d9aaa2
Omen Wild <omen@mandarb.com>
2016-06-29 16:08:16 -0700

    Add ZFS.isfilesystem() and ZFS.create_filesystem() .

---
+++ backupz/zfs.py
@@ -0,0 +1,33 @@
+"""
+The beginning of a module to handle the BackupZ ZFS calls.
+"""
+
+import collections
+
+from . import lib
+
+class ZFS(object):
+    zfs_command = '/sbin/zfs'
+    zpool_command = '/sbin/zpool'
+
+    def __init__(self):
+        pass
+
+    def isfilesystem(self, path, create=True):
+        r = lib.run_command([ZFS.zfs_command, 'list', '-o', 'name', '-H', path])
+        if create and r.rc == 1 and r.out.rstrip().endswith('dataset does not exist'):
+            return self.create_filesystem(path)
+
+        if r.rc != 0 or r.out.rstrip() != path:
+            return False
+        else:
+            return True
+
+    def create_filesystem(self, path):
+        # cannot create 'z3/omen': dataset already exists
+        # cannot create 'descolada/backups/backupz/backupsNONONO': dataset already exists
+        r = lib.run_command([ZFS.zfs_command, 'create', path])
+        if r.rc != 0 and r.err.rstrip() != "cannot create '%s': dataset already exists" % path:
+            raise RuntimeError(r.err)
+
+        return True
