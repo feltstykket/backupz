fab150b829d495651633a66a9634ea341ec4b743
Omen Wild <omen@mandarb.com>
2016-06-29 16:00:39 -0700

    Update run_command to return a namedtuple for results.
    Update run_command to look for 'permission denied' in the zpool/zfs output and raise a Python exception.

--- backupz/lib.py
+++ backupz/lib.py
@@ -1,9 +1,21 @@
 import subprocess
 import collections
 
-def run_command(command, cwd='/tmp'):
+import collections
+
+Result = collections.namedtuple('Result', 'out err rc')
+
+def run_command(command, cwd='/tmp', debug=False):
     p = subprocess.Popen(command, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-    return p.communicate()
+    out, err = p.communicate()
+    r = Result(out, err, p.returncode)
+    if debug:
+        print(r)
+
+    if 'permission denied' in err.lower():
+        raise PermissionError(err.rstrip())
+
+    return r
 
 
 def flatten(l):
